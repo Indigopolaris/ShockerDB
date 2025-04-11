d482891ac1593792c701712ff20c2c1cf6878c40
Chandler Higginbotham <morganh1234abcd@gmail.com>
2025-04-11 17:11:56 -0400

    EVEN more boiler plate

--- main.go
+++ main.go
@@ -19,30 +19,30 @@ func main() {
 
 	app.Get("/Login", func(c fiber.Ctx) error {
 		//login page
-		msg := fmt.Sprintf("Login Endpoint", c.Params(""))
+		msg := fmt.Sprintf("Login Endpoint")
 		return c.SendString(msg)
 	})
 
 	app.Get("/Dashboard", func(c fiber.Ctx) error {
 		//dashboard for the power readings, this will have the power readings over the month for each month
-		msg := fmt.Sprintf("Dashboard Endpoint", c.Params(""))
+		msg := fmt.Sprintf("Dashboard Endpoint")
 		return c.SendString(msg)
 	})
 
-	app.Get("/Input", func(c fiber.Ctx) error {
+	app.Get("/Form", func(c fiber.Ctx) error {
 		//Input form for power readings
-		msg := fmt.Sprintf("Input Data Endpoint", c.Params(""))
+		msg := fmt.Sprintf("Input Data Endpoint")
 		return c.SendString(msg)
 	})
 
-	app.Get("/Explore", func(c fiber.Ctx) error {
+	app.Get("/DataViewer", func(c fiber.Ctx) error {
 		//view all historical data
-		msg := fmt.Sprintf("Explore Data Endpoint", c.Params(""))
+		msg := fmt.Sprintf("Explore Data Endpoint")
 		return c.SendString(msg)
 	})
 
 	app.Get("/api/*", func(c fiber.Ctx) error {
-		msg := fmt.Sprintf("API Endpoint", c.Params(""))
+		msg := fmt.Sprintf("API Endpoint", c.Params("*"))
 		return c.SendString(msg)
 	})
 
